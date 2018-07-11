0e6492c666d748fef121ff754aee7d49a812918b
sathyamus <sathyamus.sify@gmail.com>
2018-07-11 14:54:37 +0530

    Added Lazy loading impl

--- myAngularApp3/src/app/app.routes.ts
+++ myAngularApp3/src/app/app.routes.ts
@@ -46,6 +46,9 @@ export const APP_ROUTES: Routes = [{
 }, {
     path: 'employee',
     component: EmployeeComponent
+}, {
+    path: 'lazy',
+    loadChildren: 'app/lazy/lazy.module#LazyModule'
 },
 {
     path: '**', //http://localhost:4200/notexist 
